02759b266b290e48a2849215c28ddd7095ea32aa
stefano <stefano.dacchille@gmail.com>
2011-07-31 21:50:45 +0200

    Added sample project.

---
+++ ignition-location-sample/src/com/github/ignition/location/example/LocationActivitySample.java
@@ -0,0 +1,31 @@
+package com.github.ignition.location.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.github.ignition.location.annotations.LocationActivity;
+
+@LocationActivity
+public class LocationActivitySample extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    }
+    
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	super.onDestroy();
+    }
+}
